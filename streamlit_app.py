1dad3836a8b41b5e19fb9f7f24d81d45126001e1
Son Dang <son_dang@epam.com>
2023-10-07 10:57:18 +0700

    Add new section for Fruitvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import requests
 
 st.header('Breakfast Favourites')
 st.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -13,7 +13,13 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
+
+# New section to display Fruivice API Response
+st.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
