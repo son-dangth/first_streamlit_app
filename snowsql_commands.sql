4a00d646f236117b533d01ddc17836a70c81c16a
Son Dang <son_dang@epam.com>
2023-10-07 15:31:44 +0700

    Remove unecessary info

--- snowsql_commands.sql
+++ snowsql_commands.sql
@@ -1,9 +1,5 @@
 show stages;
 
-put file://C:\Users\son_dang\Epam\Learn\de_snowflake\first_streamlit_app\test_internal_stage_file1.txt @UTIL_DB.PUBLIC.MY_INTERNAL_NAMED_STAGE;
-put file://C:\Users\son_dang\Epam\Learn\de_snowflake\first_streamlit_app\test_internal_stage_file2.txt @UTIL_DB.PUBLIC.MY_INTERNAL_NAMED_STAGE;
-put file://C:\Users\son_dang\Epam\Learn\de_snowflake\first_streamlit_app\test_internal_stage_file3.txt @UTIL_DB.PUBLIC.MY_INTERNAL_NAMED_STAGE;
-
 list @UTIL_DB.PUBLIC.MY_INTERNAL_NAMED_STAGE;
 
 select $1 from @UTIL_DB.PUBLIC.MY_INTERNAL_NAMED_STAGE/test_internal_stage_file1.txt;
